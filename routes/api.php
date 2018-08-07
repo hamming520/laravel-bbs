a8646fd519443bc07e211095e3f230b01771c666
achais.zheng <achais.zheng@gmail.com>
2018-08-07 21:58:55 +0800

    Update api

--- routes/api.php
+++ routes/api.php
@@ -25,13 +25,10 @@ $api->version('v1', [
         'expires' => config('api.rate_limits.sign.expires'),
     ], function($api) {
         // 短信验证码
-        $api->post('verificationCodes', 'VerificationCodesController@store')
-            ->name('api.verificationCodes.store');
+        $api->post('verificationCodes', 'VerificationCodesController@store')->name('api.verificationCodes.store');
         // 用户注册
-        $api->post('users', 'UsersController@store')
-            ->name('api.users.store');
+        $api->post('users', 'UsersController@store')->name('api.users.store');
         // 图片验证码
-        $api->post('captchas', 'CaptchasController@store')
-            ->name('api.captchas.store');
+        $api->post('captchas', 'CaptchasController@store')->name('api.captchas.store');
     });
 });
