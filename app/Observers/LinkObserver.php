fc531ddc3a27c9acd8a73daa638b779dd9180e16
achais <achais.zheng@gmail.com>
2019-01-21 20:44:34 +0800

    Update default-avatar.

--- app/Observers/LinkObserver.php
+++ app/Observers/LinkObserver.php
@@ -11,7 +11,7 @@ class LinkObserver
     {
         // 这样写扩展性更高，只有空的时候才指定默认icon
         if (empty($link->icon)) {
-        $link->icon = 'https://fsdhubcdn.phphub.org/uploads/images/201710/30/1/TrJS40Ey5k.png';
+        $link->icon = config('app.url') . '/images/default-avatar.jpeg';
     }
     }
 
